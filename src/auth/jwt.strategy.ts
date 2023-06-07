ab90032f3782b327b2399c36669d8806b5b86e90
Amaka Ibenegbu <113144586+AmakaJuliet@users.noreply.github.com>
2023-06-07 10:43:11 +0100

    commit

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secret0rKey: 'SECRET', // protect this, move to env var
+            secretOrKey: 'SECRET', // protect this, move to env var
         });
     }
 
